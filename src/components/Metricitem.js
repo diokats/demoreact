7b5e8d9923133eb1af8c55b1d4652dc02f0b6802
diokats <45209368+diokats@users.noreply.github.com>
2021-05-11 17:33:48 +0300

    Add MetricItem: dammy content

---
+++ src/components/Metricitem.js
@@ -0,0 +1,12 @@
+// function MetricItem {
+//     return
+//    ( <p>This is a Paragraph</p>)
+// } 
+// export default MetricItem
+
+
+const MetricItem = () => {
+    return <p>This is a Metric</p>
+
+}
+export default MetricItem
